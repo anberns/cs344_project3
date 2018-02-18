6b5a59ba77af1755046ee65d66133266bd7937aa
Aaron Berns <bernsa@os1.engr.oregonstate.edu>
2018-02-18 05:06:49 -0800

    execvp added

--- smallsh.c
+++ smallsh.c
@@ -16,8 +16,12 @@
 #include <pthread.h>
 #include <time.h>
 
-#define NUMROOMS 7
-
+void execute(char **argv) {
+    if (execvp(*argv, argv) < 0) {
+        perror("Exec failure");
+        exit(1);
+    }
+}
   
 int main()
 {
@@ -30,87 +34,93 @@ int main()
     char cwd[2048];
 
     char *input = NULL;
-    int charsEntered, i, exitStatus, termSignal;
+    int charsEntered, i, childExitMethod, exitStatus, termSignal;
     size_t buffersize = 0;
 
-    // spawn test process
-    pid_t spawnpid = -5;
-    int childExitMethod = -5;
     
-    spawnpid = fork();
-    switch (spawnpid)
-    {
-        case -1:
-            exit(1);
-            break;
-        case 0:
-            //for (i = 0; i < 10; ++i) {
-                printf("Yo Yo!\n");
-                sleep(1);
-            //}
-            exit(8);
-            break;
-        default:
-            waitpid(spawnpid, &childExitMethod, 0);
-            break;
-    }
-    fflush(stdin);
 
     // run shell
     while (1) {
 
         // get user command
         printf(":");
+        fflush(stdout);
         charsEntered = getline(&input, &buffersize, stdin);
 
         // remove line feed
         input[charsEntered-1] = '\0';
 
-        // parse for command and possible args
-        i = 1;
-        userArgs[0] = strtok(input, " ");
-        while (userArgs[i-1] != NULL) {
-            userArgs[i] = strtok(NULL, " ");
-            i++;
-        }
-        
-        // check for and run built in commands
-        // exit
-        if (strcmp(userArgs[0], "exit") == 0) {
+        // ignore comments and blank lines
+        if (input[0] != '#' && strcmp(input, "") != 0) {
 
-            // free memory
-            free(input);
-
-            // kill all processes, help from ibm.com
-            kill(0, 15);;
-
-            break;
-        }
-
-        // status
-        if (strcmp(userArgs[0], "status") == 0) {
+            // parse for command and possible args
+            i = 1;
+            userArgs[0] = strtok(input, " ");
+            while (userArgs[i-1] != NULL) {
+                userArgs[i] = strtok(NULL, " ");
+                i++;
+            }
             
-            //check exit status
-            if (WIFEXITED(childExitMethod) != 0) {
-                exitStatus = WEXITSTATUS(childExitMethod);    
-                printf("Exit status %d\n", exitStatus);
-            }
-            else if (WIFSIGNALED(childExitMethod) != 0) {
-                termSignal = WTERMSIG(childExitMethod);
-                printf("Terminating signal %d\n", termSignal);
-            }
-        }
+            // check for and run built in commands
+            // exit
+            if (strcmp(userArgs[0], "exit") == 0) {
 
-        // cd
-        if (strcmp(userArgs[0], "cd") == 0) {
-            if (userArgs[1] == NULL) {
-                chdir(getenv("HOME"));
+                // free memory
+                free(input);
+
+                // kill all processes, help from ibm.com
+                kill(0, 15);;
+
+                break;
             }
+
+            // status
+            else if (strcmp(userArgs[0], "status") == 0) {
+                
+                //check exit status
+                if (WIFEXITED(childExitMethod) != 0) {
+                    exitStatus = WEXITSTATUS(childExitMethod);    
+                    printf("Exit status %d\n", exitStatus);
+                }
+                else if (WIFSIGNALED(childExitMethod) != 0) {
+                    termSignal = WTERMSIG(childExitMethod);
+                    printf("Terminating signal %d\n", termSignal);
+                }
+            }
+
+            // cd
+            else if (strcmp(userArgs[0], "cd") == 0) {
+                if (userArgs[1] == NULL) {
+                    chdir(getenv("HOME"));
+                }
+                else {
+                    chdir(userArgs[1]);
+                }
+                getcwd(cwd, sizeof(cwd));
+                printf("%s\n", cwd);
+            }
+
+            // pass other commands
             else {
-                chdir(userArgs[1]);
+
+                // spawn test process
+                pid_t spawnpid = -5;
+                childExitMethod = -5;
+                
+                spawnpid = fork();
+                switch (spawnpid)
+                {
+                    case -1:
+                        exit(1);
+                        break;
+                    case 0:
+                        execute(userArgs);
+                        break;
+                    default:
+                        waitpid(spawnpid, &childExitMethod, 0);
+                        break;
+                }
             }
-            getcwd(cwd, sizeof(cwd));
-            printf("%s\n", cwd);
         }
     }
     
