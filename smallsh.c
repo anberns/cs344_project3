a0d1db6525a92cdb6947961052f5b2aa3127c056
Aaron Berns <bernsa@os1.engr.oregonstate.edu>
2018-02-18 06:57:30 -0800

    in / out redirection

--- smallsh.c
+++ smallsh.c
@@ -18,7 +18,7 @@
 
 void execute(char **argv) {
     if (execvp(*argv, argv) < 0) {
-        perror("Exec failure");
+        perror("Could not execute command");
         exit(1);
     }
 }
@@ -34,7 +34,12 @@ int main()
     char cwd[2048];
 
     char *input = NULL;
-    int charsEntered, i, childExitMethod, exitStatus, termSignal;
+    int charsEntered, i, childExitMethod, exitStatus, termSignal, redirResult,
+    sourceFD, targetFD;
+
+    // redirection command locations in userArgs[]
+    int outIndex, inIndex, backIndex, lastIndex, curIndex; 
+
     size_t buffersize = 0;
 
     
@@ -54,10 +59,30 @@ int main()
         if (input[0] != '#' && strcmp(input, "") != 0) {
 
             // parse for command and possible args
+            outIndex = inIndex = backIndex = 0;
             i = 1;
             userArgs[0] = strtok(input, " ");
             while (userArgs[i-1] != NULL) {
                 userArgs[i] = strtok(NULL, " ");
+
+                // look for <, >, & and store index
+                if (userArgs[i] != NULL) {
+                    if (strcmp(userArgs[i], "<") == 0) {
+                        inIndex = i;
+                      //  printf("inIndex = %d\n", inIndex);
+                    } else if (strcmp(userArgs[i], ">") == 0) {
+                        outIndex = i;
+                     //   printf("outIndex = %d\n", outIndex);
+                    } else if (strcmp(userArgs[i], "&") == 0) {
+                        backIndex = i;
+                    }
+
+                    // keep track of last arg
+                    lastIndex = i+1;
+                    //printf("lastIndex = %d\n", lastIndex);
+
+                }
+
                 i++;
             }
             
@@ -107,6 +132,7 @@ int main()
                 pid_t spawnpid = -5;
                 childExitMethod = -5;
                 
+                int stopLoop = 10;
                 spawnpid = fork();
                 switch (spawnpid)
                 {
@@ -114,6 +140,65 @@ int main()
                         exit(1);
                         break;
                     case 0:
+                        // perform redirection if needed
+                        // output
+                        if (outIndex != 0) {
+                            targetFD = open(userArgs[outIndex + 1], O_WRONLY |
+                            O_CREAT | O_TRUNC, 0644);
+                            if (targetFD == -1) {
+                                perror("target open()"); 
+                                exit(1); 
+                            }
+                            redirResult = dup2(targetFD, 1);
+                            if (redirResult == -1) {
+                                perror("target dup2()");
+                                exit(2);
+                            }
+
+                            // remove >, target name from userArgs[]
+                            userArgs[outIndex] = NULL;
+                            userArgs[outIndex + 1] = NULL;
+                            if (outIndex + 2 != lastIndex) {
+                                curIndex = outIndex;
+                                while (curIndex + 2 != lastIndex && stopLoop != 0) {
+                                    userArgs[curIndex] = userArgs[curIndex + 2];
+                                    userArgs[curIndex + 2] = NULL;
+                                    curIndex++;
+                                    stopLoop--;
+                                }
+                                lastIndex -= 2;
+                            }
+                        }
+
+                        // input 
+                        if (inIndex != 0) {
+                            sourceFD= open(userArgs[inIndex + 1], O_RDONLY);
+                            if (sourceFD == -1) {
+                                perror("source open()"); 
+                                exit(1); 
+                            }
+                            redirResult = dup2(sourceFD, 0);
+                            if (redirResult == -1) {
+                                perror("source dup2()");
+                                exit(2);
+                            }
+
+                            // remove <, source name from userArgs[]
+                            userArgs[inIndex] = NULL;
+                            userArgs[inIndex + 1] = NULL;
+                            stopLoop = 10;
+                            if (inIndex + 2 != lastIndex && stopLoop != 0) {
+                                curIndex = inIndex;
+                                while (curIndex + 2 != lastIndex) {
+                                    userArgs[curIndex] = userArgs[curIndex + 2];
+                                    userArgs[curIndex + 2] = NULL;
+                                    curIndex++;
+                                    stopLoop--;
+                                }
+                                lastIndex -= 2;
+                            }
+                        }
+                        
                         execute(userArgs);
                         break;
                     default:
@@ -123,7 +208,6 @@ int main()
             }
         }
     }
-    
 
 	return 0;
 }
